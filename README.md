5ac3366b596f58af79186b7e1afabdc79d7cf0bf
Samdeep Sharma <sharmasamdeep1@gmail.com>
2024-04-02 00:02:36 +0530

    Initial frontend commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# iNotebook - A secure place to save your notes
+
+## Save notes on the cloud
+## Stay secure and private with iNotebook
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>This is iNotebook</h1>
+    </>
+  )
+}
+
+export default App
